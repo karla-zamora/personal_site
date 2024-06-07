1f84351682018d8f93d4b9cb30cea9b9dd03f219
Karla-zamora <karla.zamora01@utrgv.edu>
2024-06-07 12:38:53 -0500

    Test turtle

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://i.natgeofe.com/n/671ea18d-3185-42a9-8e64-a8470befbe11/01-turtle-day-photo-ark_4x3.jpg"} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test 
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="SunButton"
+          href="https://www.google.com/search?q=sun&rlz=1C1UUXU_esMX992MX992&oq=sun&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORiABDIMCAEQIxgnGIAEGIoFMgcIAhAuGIAEMhMIAxAuGIMBGMcBGLEDGNEDGIAEMgoIBBAAGMkDGIAEMgYIBRBFGDwyBggGEEUYPDIGCAcQRRg80gEHNjEwajBqOagCALACAQ&sourceid=chrome&ie=UTF-8"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GET SUN'D
         </a>
       </header>
     </div>
