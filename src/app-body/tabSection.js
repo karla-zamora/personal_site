3d189e9694ac49b887b91306f44a4b490f6160eb
karla-zamora <karla.zamora01@utrgv.edu>
2024-07-26 04:30:10 -0500

    finished background, started projects carousel

--- src/app-body/tabSection.js
+++ src/app-body/tabSection.js
@@ -4,6 +4,8 @@ import './tabSection.css'
 import { useEffect, useRef, useState } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import Carousel from 'react-bootstrap/Carousel';
+
 
 function TabSection() {
   // return to top of tab when switching tabs
@@ -14,6 +16,10 @@ function TabSection() {
       tabsRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   }, [activeKey]);
+
+  // projects array for easier storage
+  const projectPic = 'https://colourlex.com/wp-content/uploads/2021/02/vine-black-painted-swatch.jpg';
+  const projectAlt = 'tempAlt';
 
   return (
     <div>
@@ -26,40 +32,130 @@ function TabSection() {
         onSelect={(k) => setActiveKey(k)}
       >
         <Tab eventKey="background" title="Background" className="Tab">
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
-          <p>Nice to meet you!</p>
+
+          <section class="bsb-timeline-1 py-5 py-xl-8">
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-10 col-md-8 col-xl-6">
+
+                  <ul id="timelineLine" class="timeline">
+                    <li class="timeline-item">
+                      <div class="timeline-body">
+                        <div class="timeline-content">
+                          <div class="card border-0">
+                            <div class="card-body p-2">
+                              <h5 class="card-subtitle text-secondary m-2">2019</h5>
+                              <p class="card-text mb-2">Didn't own a laptop at the time. Become interested in programming and start learning C++ on an android compiler.</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li class="timeline-item">
+                      <div class="timeline-body">
+                        <div class="timeline-content">
+                          <div class="card border-0">
+                            <div class="card-body p-2">
+                              <h5 class="card-subtitle text-secondary m-2">2020</h5>
+                              <p class="card-text mb-2">Take my first C++ class as an Electrical Engineering student. Absolutely fall in love with coding and decide to switch majors!</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li class="timeline-item">
+                      <div class="timeline-body">
+                        <div class="timeline-content">
+                          <div class="card border-0">
+                            <div class="card-body p-2">
+                              <h5 class="card-subtitle text-secondary m-2">2022</h5>
+                              <p class="card-text mb-2">Build my first "hangman" website. Start researching web dev and gaming frameworks to build small projects.</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li class="timeline-item">
+                      <div class="timeline-body">
+                        <div class="timeline-content">
+                          <div class="card border-0">
+                            <div class="card-body p-2">
+                              <h5 class="card-subtitle text-secondary m-2">2023</h5>
+                              <p class="card-text mb-2">Get my first CS related job as a Data Structures and Algorithms tutor and become a pro at solving coding challenges.</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li class="timeline-item">
+                      <div class="timeline-body">
+                        <div class="timeline-content">
+                          <div class="card border-0">
+                            <div class="card-body p-2">
+                              <h5 class="card-subtitle text-secondary m-2">Summer 2024</h5>
+                              <p class="card-text mb-2">Graduate as a Computer Engineer. Get my first programming job at Outlier.AI, obtain certifications from Codepath's technical courses, and become a SWE fellow at Headstarter AI.</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li id="timeline" class="timeline-item">
+                      <div class="timeline-body">
+                        <div class="timeline-content">
+                          <div class="card border-0">
+                            <div class="card-body p-2">
+                              <h5 class="card-subtitle text-secondary m-2">Fall 2024</h5>
+                              <h2 class="card-title mb-3">Next Milestone</h2>
+                              <p class="card-text mb-2">Land a Software Engineering role and become an AI Engineer!</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+
+                </div>
+              </div>
+            </div>
+          </section>
         </Tab>
-        <Tab eventKey="resume" title="Resume">
+
+        <Tab eventKey="resume" title="Resume" className="Tab">
           <p>Current resume:</p>
-          <a href={resumePic} target="_blank"
-            rel="noopener noreferrer">
-            <img src={resumePic} alt="karla zamora resume" className='Resume-pic' />
-          </a>
+          <div>
+            <a href={resumePic} target="_blank"
+              rel="noopener noreferrer" align='right'>
+              <img src={resumePic} alt="karla zamora resume" className='Resume-pic p-2' />
+            </a>
+          </div>
+
 
         </Tab>
-        <Tab eventKey="projects" title="Projects">
-          Tab content for Loooonger Tab
+        <Tab eventKey="projects" title="Projects" className="Tab">
+          <Carousel id="project-crl">
+            <Carousel.Item>
+              <img src={projectPic} alt={projectAlt}/>
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={projectPic} alt={projectAlt} />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </Tab>
-        <Tab eventKey="contact" title="Contact">
+
+        <Tab eventKey="contact" title="Contact" className="Tab">
           <p>Nice to meet you!</p>
           <p>Nice to meet you!</p>
           <p>Nice to meet you!</p>
@@ -83,6 +179,9 @@ function TabSection() {
           <p>Nice to meet you!</p>
         </Tab>
       </Tabs>
+      <div className='Tab-Free-zone'>
+        <hr></hr>
+      </div>
     </div>
   );
 }
