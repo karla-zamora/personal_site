f9c5330ef519d25c78037303c5e692affae3feda
Karla-zamora <karla.zamora01@utrgv.edu>
2025-03-29 06:47:30 -0500

    background, and resume-(mobile ready)

---
+++ src/app-body/Sections/Background.js
@@ -0,0 +1,101 @@
+const Background = () => {
+  return (
+    <section class="bsb-timeline-1 py-5 py-xl-8">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-10 col-md-8 col-xl-6">
+
+            <ul id="timelineLine" class="timeline">
+              <li class="timeline-item">
+                <div class="timeline-body">
+                  <div class="timeline-content">
+                    <div class="card border-0">
+                      <div class="card-body p-2">
+                        <h5 class="card-subtitle text-secondary m-2">2019</h5>
+                        <p class="card-text mb-2">Didn't own a laptop at the time. Become interested in programming and start learning C++ on an android compiler.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li class="timeline-item">
+                <div class="timeline-body">
+                  <div class="timeline-content">
+                    <div class="card border-0">
+                      <div class="card-body p-2">
+                        <h5 class="card-subtitle text-secondary m-2">2020</h5>
+                        <p class="card-text mb-2">Take my first C++ class as an Electrical Engineering student. Absolutely fall in love with coding and decide to switch majors!</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li class="timeline-item">
+                <div class="timeline-body">
+                  <div class="timeline-content">
+                    <div class="card border-0">
+                      <div class="card-body p-2">
+                        <h5 class="card-subtitle text-secondary m-2">2022</h5>
+                        <p class="card-text mb-2">Build my first "hangman" website. Start researching web dev and gaming frameworks to build small projects.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li class="timeline-item">
+                <div class="timeline-body">
+                  <div class="timeline-content">
+                    <div class="card border-0">
+                      <div class="card-body p-2">
+                        <h5 class="card-subtitle text-secondary m-2">2023</h5>
+                        <p class="card-text mb-2">Get my first CS related job as a Data Structures and Algorithms tutor and become a pro at solving coding challenges.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li class="timeline-item">
+                <div class="timeline-body">
+                  <div class="timeline-content">
+                    <div class="card border-0">
+                      <div class="card-body p-2">
+                        <h5 class="card-subtitle text-secondary m-2">Summer 2024</h5>
+                        <p class="card-text mb-2">
+                          <li><p className='Main-point m-0'>Graduate as a Computer Engineer</p><li>University of Texas Rio Grande Valley</li><li>Magna Cum Laude</li><li>GPA: 3.87</li></li>
+                          <br></br><li><p className='Main-point m-0'>Get my first programming job</p><li>AI Training Coder at Outlier.AI</li></li>
+                          <br></br><li><p className='Main-point m-0'>Obtain Certifications</p><li>Codepath's Technical Interview Prep</li></li>
+                          <br></br><li><p className='Main-point m-0'>Become a Software Engineering Fellow</p><li>Headstarter AI</li></li>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li id="timeline" class="timeline-item">
+                <div class="timeline-body">
+                  <div class="timeline-content">
+                    <div class="card border-0">
+                      <div class="card-body p-2">
+                        <h5 class="card-subtitle text-secondary m-2">Fall 2024</h5>
+                        <h2 class="card-title mb-3">Next Milestone</h2>
+                        <p class="card-text mb-2">Land a Software Engineering role and become an AI Engineer!</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Background;
---
+++ src/app-body/Sections/Resume.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+export default function Resume({ resumeFile }) {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 992);
+  console.log(isMobile);
+
+  return (
+    <div>
+      <p>Current resume:</p>
+      <p style={{ fontSize: "10px" }}>This page is still in progress, so resume may not display correctly!</p>
+      <div>
+        {!isMobile ? (
+          <iframe 
+            title="resume"
+            width="100%"
+            style={{
+              height: "75rem",
+              border: "none",
+            }}
+            alt="karla zamora resume" 
+            className="Resume-pic p-2 w-screen h-screen" src={resumeFile} />
+        ) : (
+          <iframe
+            title="resume"
+            className="w-100"
+            style={{
+              height: "130vw",
+              border: "none",
+            }}
+            src={`https://docs.google.com/gview?url=${encodeURIComponent(resumeFile)}&embedded=true`}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
