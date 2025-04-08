03ffd8855a26670fe7c256aaf9de9d88c9517a5f
Karla-zamora <karla.zamora01@utrgv.edu>
2025-04-08 07:42:28 -0500

    some browsers dont display gdocs so i just fixed an image on mobile

--- src/app-body/Sections/Resume.js
+++ src/app-body/Sections/Resume.js
@@ -1,33 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import resumePic from '../../images/resume-pic.jpg';
 
 export default function Resume({ resumeFile }) {
-  const [isMobile] = useState(window.innerWidth < 992);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 992);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 992);
+    };
+
+    window.addEventListener('resize', handleResize);
+    
+    // Cleanup function to remove event listener
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div>
       <p>Current resume:</p>
-      <div>
+      <div className="text-center d-flex flex-column align-items-center">
         {!isMobile ? (
           <iframe 
             title="resume"
             width="100%"
             style={{
               height: "75rem",
+              width: "55rem",
+              maxWidth: "90vw",
               border: "none",
             }}
             alt="karla zamora resume" 
             className="Resume-pic p-2 w-screen h-screen" src={resumeFile ? `${resumeFile}?#zoom=100&scrollbar=1&toolbar=1&navpanes=0` : ""} />
         ) : (
-          <iframe
-            title="resume"
-            className="w-100"
-            style={{
-              height: "130vw",
-              border: "none",
-            }}
-            src={`https://docs.google.com/gview?url=${encodeURIComponent(resumeFile)}&embedded=true`}
-          />
+          <img src={resumePic} width="90%" alt="karla zamora resume" />
         )}
+        <button onClick={() => window.open(resumeFile, '_blank')}>Open Resume</button>
       </div>
     </div>
   );
